dfbb4a690cefd2c2f409dc8030b77fc732b36bdd
sivasai <sivasainookala@gmail.com>
2023-10-02 17:40:41 +0530

    PRODIGY_WB_01

---
+++ script.js
@@ -0,0 +1,24 @@
+
+
+// mousemove home img
+
+document.addEventListener('mousemove', move);
+function move(e){
+  this.querySelectorAll('.move').forEach(layer =>{
+    const speed = layer.getAttribute('data-speed')
+
+    const x = (window.innerWidth - e.pageX*speed)/100
+    const y = (window.innerWidth - e.pageY*speed)/100
+
+    layer.style.transform = `translateX(${x}px) translateY(${y}px)`
+
+  })
+}
+
+gsap.from('.logo', {opacity: 0, duration: 1, delay: 2, y:10})
+gsap.from('.navbar .nav_item', {opacity: 0, duration: 1, delay: 2.1, y:30, stagger: 0.2})
+
+gsap.from('.title', {opacity: 0, duration: 1, delay: 1.6, y:30})
+gsap.from('.description', {opacity: 0, duration: 1, delay: 1.8, y:30})
+gsap.from('.button', {opacity: 0, duration: 1, delay: 2.1, y:30})
+gsap.from('.image', {opacity: 0, duration: 1, delay: 2.6, y:30})
